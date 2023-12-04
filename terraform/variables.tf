0cee30765fe6d8c464f49ed045dbfd99512d7d9e
mly219 <mly219@163.com>
2023-12-04 23:41:30 +0800

    commit terraform file to create a vm

---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default = "kenny"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default = "Japan East"
+}
+
+variable "username" {
+  description = "user name"
+  default = "azureuser"
+}
+
+variable "password" {
+  description = "password."
+  default = "Zs850605:)"
+}
