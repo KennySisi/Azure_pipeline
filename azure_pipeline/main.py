19c0a4be42e53230c012952ebc37c7b93b9c342c
mly219 <mly219@163.com>
2024-03-22 07:09:41 +0800

    minor change

--- azure_pipeline/main.py
+++ azure_pipeline/main.py
@@ -5,9 +5,14 @@ app = FastAPI()
 
 @app.get("/")
 def rootFunction():
-    return "Hello, visitor"
+    return "Hello, visitor new deploy triggered"
 
 
 @app.get("/Add/{number1}")
 def add_two(number1):
     return {f"Your input is: {number1}"}
+
+
+@app.get("/Delete/{number1}")
+def add_two(number1):
+    return {f"Delete Number: {number1}"}
