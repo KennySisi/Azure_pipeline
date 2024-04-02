48e547d8bc08343f7be9463f817205aa7340fd29
Kenny Mo <kmo@rockwellautomation.com>
2024-04-02 16:07:37 +0800

    support query storage account container names

--- azure_pipeline/main.py
+++ azure_pipeline/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 import pyodbc
 # from azure.applicationinsights import applicationinsights
+
+from azure.storage.blob import BlobServiceClient
+
 app = FastAPI()
 # instrumentation_key = os.environ.get('INSTRUMENTATION_KEY')
 # if instrumentation_key:
@@ -34,6 +37,15 @@ def queryDataBase():
 
     return str(rows)
 
+@app.get("/storagetest")
+def queryStorageAccount():
+    account_name = "stvmkennymyappea"
+    account_key = "+cXhEDQmxmUafIp4qHtc7qkx7GdRwUXBrdec1bfJveOfyv5Wb6dLa9kAI/Y8uuBFXWBUjhZE4+PV+AStrzKApQ=="
+    container_name = "testcontainer"
+    blob_service_client = BlobServiceClient(f"https://{account_name}.blob.core.windows.net/", credential=account_key)
+    container_client = blob_service_client.get_container_client(container_name)
+    blob_list = container_client.list_blob_names()
+    return blob_list
 
 @app.get("/")
 def rootFunction():
--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
         'fastapi==0.104.1',
         'uvicorn==0.24.0.post1',
         'pyodbc==5.1.0',
+        'azure-storage-blob==12.19.1',
         'azure-applicationinsights>=0.1.1'
     ],
 )
