82b942a2e8d77a94f71fb83d98c9b997f477f1d6
Kenny Mo <kmo@rockwellautomation.com>
2024-04-04 21:35:33 +0800

    use managed system identity

--- azure_pipeline/main.py
+++ azure_pipeline/main.py
@@ -10,6 +10,17 @@ app = FastAPI()
 #     azure_app_insights = applicationinsights(instrumentation_key=instrumentation_key)
 #     azure_app_insights.init_app(app)
 
+# from azure.applicationinsights import applicationinsights
+
+# @app.get("/storagetest")
+# def queryStorageAccount():
+#     account_name = "stvmkennymyappea"
+#     container_name = "testcontainer"
+#     blob_service_client = BlobServiceClient(f"https://{account_name}.blob.core.windows.net/", credential=)
+#     container_client = blob_service_client.get_container_client(container_name)
+#     blob_list = container_client.list_blob_names()
+#     return blob_list
+
 @app.get("/Add/{number1}")
 def add_two(number1):
     return {f"backend2: Your input is: {number1}"}
