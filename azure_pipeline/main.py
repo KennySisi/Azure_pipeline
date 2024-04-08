ae6d44f916754381ebaff4ef0cc0a78dc6115c39
Kenny Mo <kmo@rockwellautomation.com>
2024-04-08 15:17:28 +0800

    ping redis cache

--- azure_pipeline/main.py
+++ azure_pipeline/main.py
@@ -13,7 +13,12 @@ from azure.identity import DefaultAzureCredential
 # from azure.mgmt.sql import SqlManagementClient
 
 import redis
-redis_cache_with_password = redis.StrictRedis("kenny.redis.cache.windows.net", password=os.environ.get("REDIS_ACCESS_KEY"))
+
+password=os.environ.get('REDIS_ACCESS_KEY')
+redis_cache_with_password = redis.StrictRedis("kenny.redis.cache.windows.net", password=password)
+result_ping = redis_cache_with_password.ping()
+if result_ping:
+    print("Ping returned : " + str(result_ping))
 credential = DefaultAzureCredential()
 
 subscription_id = 'd90899a9-7716-4f55-88fe-22720fe4d18a'
